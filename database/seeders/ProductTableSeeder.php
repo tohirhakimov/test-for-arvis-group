b22c37d23a572140da97790b482c95f97da0148d
Tohir <hakimovtohirbek@gmail.com>
2022-01-14 17:47:28 +0500

    generate factory and seeder for Product table

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ProductTableSeeder::class);
+        Product::factory()->count(50)->create();
     }
 }
